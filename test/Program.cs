fa92876efb6f7923156289108b59854976d04d74
Jacek Kawa <jacek.kawa@polsl.pl>
2020-03-26 02:37:53 +0100

    wersja z zakodowanymi parametrami

--- test/Program.cs
+++ test/Program.cs
@@ -6,7 +6,12 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a = 1, b = 0, c = -25;
+            double delta = b * b - 4 * a * c;
+            double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
+            double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
+
+            Console.WriteLine(String.Format("x1={0}, x2={1}", x1, x2));
         }
     }
 }
